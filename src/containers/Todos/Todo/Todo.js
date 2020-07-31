456387dc31f0392faebe1efd5c3178c198384f56
samaromm <samaromm98@gmail.com>
2020-07-31 23:56:30 +0300

    edit form

--- src/containers/Todos/Todo/Todo.js
+++ src/containers/Todos/Todo/Todo.js
@@ -40,10 +40,21 @@ class AddTodo extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      isEditing:false,
+      editTodo:{},
       isOpened: false,
       startVal: "Tue July 21 2020 19:30:00 GMT+0300 (Arabian Standard Time)",
       endVal: "Wed July 22 2020 19:30:00 GMT+0300 (Arabian Standard Time)",
     };
+  }
+
+  loadingText = "Adding...";
+
+  editEvent=({event})=>{
+    this.setState({
+      isEditing:true,
+      editTodo:event
+    },this.open())
   }
 
   toggleAddModal = ({ start, end }) => {
@@ -64,36 +75,42 @@ class AddTodo extends React.Component {
     return (
       <Modal opened={this.state.isOpened} close={() => this.open()}>
         <Heading noMargin size="h1" color="white">
-          Add your new todo
+          {this.state.isEditing ? "Edit your todo" : "Add your new todo"}
         </Heading>
+        {console.log(this.state.editTodo)}
         <Heading bold size="h4" color="white">
-          Type your todo and press add
+          {this.state.isEditing
+            ? "Edit your todo and tap edit"
+            : "Type your todo and press add"}
         </Heading>
         <Formik
           initialValues={{
-            title: "",
-            start: this.state.startVal,
-            end: this.state.endVal,
-            diary: "",
-            complete: "Uncompleted",
+            title: this.state.isEditing ? this.state.editTodo.title : "",
+            start: this.state.isEditing ? this.state.editTodo.start :this.state.startVal,
+            end: this.state.isEditing ? this.state.editTodo.end :this.state.endVal,
+            diary: this.state.isEditing ? this.state.editTodo.diary :"",
+            complete: this.state.isEditing? this.state.editTodo.complete :"Uncompleted",
           }}
           validationSchema={TodoSchema}
           onSubmit={async (values, { setSubmitting, resetForm }) => {
-            const res = await this.props.addTodo(values);
-            setSubmitting(false);
+            const res = this.state.isEditing
+              ? await this.props.editTodoAction(this.state.editTodo.id, values)
+              : await this.props.addTodo(values);
             if (res) {
               this.setState({
                 isOpened: false,
+                isEditing:false
               });
             }
+            setSubmitting(false);
             resetForm();
           }}
         >
-          {({ isSubmitting, isValid }) => (
+          {({ isSubmitting, isValid, resetForm }) => (
             <StyledForm>
               <Field
                 type="text"
-                name="todo"
+                name="title"
                 placeholder="Write your todo..."
                 component={Input}
               />
@@ -105,7 +122,7 @@ class AddTodo extends React.Component {
                 style={{ height: "100px" }}
               />
               <div className="radioButWrapper">
-                <label for="Uncompleted" >
+                <label for="Uncompleted">
                   <Field
                     type="radio"
                     name="complete"
@@ -135,11 +152,11 @@ class AddTodo extends React.Component {
                   color="main"
                   type="submit"
                   disabled={!isValid || isSubmitting}
-                  loading={this.props.loading ? "Adding..." : null}
+                  loading={this.props.loading ? this.props.loadingText : null}
                 >
-                  Add todo
+                  {this.state.isEditing ? 'Edit todo' : 'Add todo'}
                 </Button>
-                <Button color="main" contain onClick={() => this.open()}>
+                <Button color="main" contain onClick={() => {this.open();  resetForm();}}>
                   Cancel
                 </Button>
               </ButtonsWrapper>
@@ -166,23 +183,21 @@ class AddTodo extends React.Component {
           views={["month", "agenda"]}
           scrollToTime={new Date(1970, 1, 1, 6)}
           defaultDate={moment().toDate()}
-          onSelectEvent={(event) => alert(event.title)}
+          onSelectEvent={(event) => this.editEvent({event})}
           onSelectSlot={this.toggleAddModal}
           style={{ height: "100vh", margin: "20px", width: "100vh" }}
-          eventPropGetter={
-            (event, start, end, isSelected) => {
-              let newStyle = {
-                backgroundColor: "#F43F54",
-              };
-              if (event.complete==="Completed"){
-                newStyle.backgroundColor = "#1ec91e"
-              }
-              return {
-                className: "",
-                style: newStyle
-              };
+          eventPropGetter={(event, start, end, isSelected) => {
+            let newStyle = {
+              backgroundColor: "#F43F54",
+            };
+            if (event.complete === "Completed") {
+              newStyle.backgroundColor = "#1ec91e";
             }
-          }
+            return {
+              className: "",
+              style: newStyle,
+            };
+          }}
         />
         {this.state.isOpened ? this.openModal() : ""}
       </>
@@ -197,6 +212,7 @@ const mapStateToProps = ({ todos }) => ({
 
 const mapDispatchToProps = {
   addTodo: actions.addTodo,
+  editTodoAction: actions.editTodo,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(AddTodo);
