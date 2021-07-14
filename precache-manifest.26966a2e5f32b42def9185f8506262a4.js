da81f7a9ae06eb23e5606a4c700cfa6eb400c911
samaromm <samaromm98@gmail.com>
2021-07-14 21:56:18 +0300

    Updates

---
+++ precache-manifest.26966a2e5f32b42def9185f8506262a4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9bc3476ca012ab2220bd3f300701140",
+    "url": "/page-of-youth/index.html"
+  },
+  {
+    "revision": "daafcbcf5ae4aa095e08",
+    "url": "/page-of-youth/static/css/4.e46185bd.chunk.css"
+  },
+  {
+    "revision": "0a1be001060153ea5d2e",
+    "url": "/page-of-youth/static/css/main.12a6c647.chunk.css"
+  },
+  {
+    "revision": "d5345adfed5b9f9bb4f2",
+    "url": "/page-of-youth/static/js/2.cdaaf6ff.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/page-of-youth/static/js/2.cdaaf6ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946f22e7a70d1918f8ba",
+    "url": "/page-of-youth/static/js/3.e1e28dd0.chunk.js"
+  },
+  {
+    "revision": "000d6c36287224a7ff48f88e64636454",
+    "url": "/page-of-youth/static/js/3.e1e28dd0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daafcbcf5ae4aa095e08",
+    "url": "/page-of-youth/static/js/4.91072599.chunk.js"
+  },
+  {
+    "revision": "0a1be001060153ea5d2e",
+    "url": "/page-of-youth/static/js/main.675c887c.chunk.js"
+  },
+  {
+    "revision": "93ec747c3264ed79d207",
+    "url": "/page-of-youth/static/js/runtime-main.98394b7e.js"
+  }
+]);
